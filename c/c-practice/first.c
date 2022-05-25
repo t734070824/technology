82250b8c5833ba1b5fa9405ab1a686e982b409e5
tangqing <734070824@qq.com>
2022-05-25 13:51:03 +0800

    first

---
+++ c/c-practice/first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int num;
+    num = 1;
+    printf("i am a simple");
+    printf("comp. \n");
+    printf("mycat fa number is %d, it is first. \n", num);
+    return 0;
+}
