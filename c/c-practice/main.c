6b7e6dabfad9aea748fd03ef7cb0cad573116625
t734070824 <734070824@qq.com>
2022-06-01 09:48:27 +0800

    csapp test

--- c/c-practice/main.c
+++ c/c-practice/main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
+typedef  unsigned char *byte_pointer;
 
-#define MSG "Im special"
+void show_bytes(byte_pointer start, size_t len){
+    size_t i;
+    for(i = 0;i < len;i++){
+        printf(" %.2x", start[i]);
+    }
+    printf("\n");
+}
+
+void show_int(int x){
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x){
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x){
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
 
 int main(void) {
-    char ar[] = MSG;
-    const char  *pt = MSG;
-    printf("address of im %p \n", "Im special");
-    printf("address ar : %p \n", ar);
-    printf("address pt : %p \n", pt);
-    printf("address MSG : %p \n", MSG);
-    printf("address of im %p \n", "Im special");
+    int i = 3510593;
+    float  f = 3510593.0;
+    show_int(i);
+    show_float(f);
     return 0;
 }
 
